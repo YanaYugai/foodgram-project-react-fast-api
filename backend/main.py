01e2d60ada83e9be4958c4781ffe170978144055
Яна Югай <yanayugai6@gmail.com>
2024-01-10 18:23:32 +0300

    Try alembic with asyncpg

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+app = FastAPI()
 
 class Item(BaseModel):
     name: str
@@ -9,11 +10,3 @@ class Item(BaseModel):
     tax: float | None = None
     app = FastAPI()
 
-
-@app.post("/items/")
-async def create_item(item: Item):
-    item_dict = item.dict()
-    if item.tax:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict
