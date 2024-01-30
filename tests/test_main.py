a67ad22995cd7f4e8628a5d03e85b737bb927905
Яна Югай <yanayugai6@gmail.com>
2024-01-30 19:05:55 +0300

    Add test for recipes

--- tests/test_main.py
+++ tests/test_main.py
@@ -83,6 +83,34 @@ def test_update_recipe():
 
 def test_update_recipe_uncorrect_value():
     "Невозможно обновить рецепт с переданными некорректными данными"
+    json={
+            "ingredients": [
+                {
+                    "id": 1123,
+                    "amount": 10
+                }
+            ],
+            "tags": [3],
+            "image": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAgMAAABieywaAAAACVBMVEUAAAD///9fX1/S0ecCAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAACklEQVQImWNoAAAAggCByxOyYQAAAABJRU5ErkJggg==",
+            "name": "string",
+            "text": "string",
+            "cooking_time": 1
+        }
+    for key, value in json.values():
+        del json[key]
+        response = client.patch(
+            "/api/recipes/1/",
+            headers={"Authorization": "Token TOKENVALUE"},
+            json=json
+        )
+        assert response.status_code == 400, "Возможно изменить рецепт с некорректным количеством ингридиентов"
+        assert response.json() == {
+            key: [
+                "Обязательное поле."
+            ]
+        }
+        json[key] = value
+
     response = client.patch(
         "/api/recipes/1/",
         headers={"Authorization": "Token TOKENVALUE"},
@@ -110,23 +138,6 @@ def test_update_recipe_uncorrect_value():
                     ]
                 },
             ],
-}
-    response = client.patch(
-        "/api/recipes/1/",
-        headers={"Authorization": "Token TOKENVALUE"},
-        json={
-            "tags": [3],
-            "image": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAgMAAABieywaAAAACVBMVEUAAAD///9fX1/S0ecCAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAACklEQVQImWNoAAAAggCByxOyYQAAAABJRU5ErkJggg==",
-            "name": "string",
-            "text": "string",
-            "cooking_time": 1
-        }
-    )
-    assert response.status_code == 400, "Возможно изменить рецепт без обязательного поля"
-    assert response.json() == {
-        "ingredients": [
-            "Обязательное поле."
-        ]
     }
 
 
