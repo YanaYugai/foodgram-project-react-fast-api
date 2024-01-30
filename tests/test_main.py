a2de0c3e1a873d85454c6acb623ab40a8a4ce489
Яна Югай <yanayugai6@gmail.com>
2024-01-30 17:59:54 +0300

    Add test for recipes

---
+++ tests/test_main.py
@@ -0,0 +1,146 @@
+from fastapi.testclient import TestClient
+
+from backend.app.main import app
+from backend.app.temporary_db import users, recipes
+
+
+client = TestClient(app)
+
+
+def test_create_recipe():
+    response = client.post(
+        "/api/recipes/",
+        headers={"Authorization": "Token TOKENVALUE"},
+        json={
+            "ingredients": [
+                {
+                    "id": 1123,
+                    "amount": 10
+                }
+            ],
+            "tags": [1,2],
+            "image": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAgMAAABieywaAAAACVBMVEUAAAD///9fX1/S0ecCAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAACklEQVQImWNoAAAAggCByxOyYQAAAABJRU5ErkJggg==",
+            "name": "string",
+            "text": "string",
+            "cooking_time": 1
+        }
+    )
+    assert response.status_code == 200
+    assert response.json() == recipes[:-1]
+
+
+def test_create_recipe_failure():
+    response = client.post(
+        "/api/recipes/",
+        headers={"Authorization": "Token invalid_token"},
+        json={
+            "ingredients": [
+                {
+                    "id": 11,
+                    "amount": 10
+                }
+            ],
+            "tags": [2],
+            "image": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAgMAAABieywaAAAACVBMVEUAAAD///9fX1/S0ecCAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAACklEQVQImWNoAAAAggCByxOyYQAAAABJRU5ErkJggg==",
+            "name": "string",
+            "text": "string",
+            "cooking_time": 1
+        }
+    )
+    assert response.status_code == 401
+    assert response.json() == {
+        "detail": "Учетные данные не были предоставлены."
+    }
+
+
+def test_get_recipe():
+    response = client.get('/api/recipes/1')
+    assert response.status_code==200
+    assert response.json=={
+        "id": 0,
+        "tags": [
+            {
+            "id": 0,
+            "name": "Завтрак",
+            "color": "#E26C2D",
+            "slug": "breakfast"
+            }
+        ],
+        "author": {
+            "email": "user@example.com",
+            "id": 0,
+            "username": "string",
+            "first_name": "Вася",
+            "last_name": "Пупкин",
+            "is_subscribed": False
+        },
+        "ingredients": [
+            {
+            "id": 0,
+            "name": "Картофель отварной",
+            "measurement_unit": "г",
+            "amount": 1
+            }
+        ],
+        "is_favorited": True,
+        "is_in_shopping_cart": True,
+        "name": "string",
+        "image": "http://foodgram.example.org/media/recipes/images/image.jpeg",
+        "text": "string",
+        "cooking_time": 1
+}
+
+
+def test_update_recipe():
+    response = client.post(
+        "/api/recipes/1/",
+        headers={"Authorization": "Token TOKENVALUE"},
+        json={
+            "ingredients": [
+                {
+                    "id": 1123,
+                    "amount": 10
+                }
+            ],
+            "tags": [3],
+            "image": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAgMAAABieywaAAAACVBMVEUAAAD///9fX1/S0ecCAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAACklEQVQImWNoAAAAggCByxOyYQAAAABJRU5ErkJggg==",
+            "name": "string",
+            "text": "string",
+            "cooking_time": 1
+        }
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": 0,
+        "tags": [
+            {
+                "id": 1,
+                "name": "Завтрак",
+                "color": "#E26C2D",
+                "slug": "breakfast"
+            }
+        ],
+        "author": {
+            "email": "user@example.com",
+            "id": 0,
+            "username": "string",
+            "first_name": "Вася",
+            "last_name": "Пупкин",
+            "is_subscribed": False
+            },
+            "ingredients": [
+                {
+                    "id": 0,
+                    "name": "Картофель отварной",
+                    "measurement_unit": "г",
+                    "amount": 1
+                }
+            ],
+            "is_favorited": True,
+            "is_in_shopping_cart": True,
+            "name": "string",
+            "image": "http://foodgram.example.org/media/recipes/images/image.jpeg",
+            "text": "string",
+            "cooking_time": 1
+    }
+
