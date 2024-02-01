99bb2b1fa5fc5491ec4dd48d776d997e357bc5be
Яна Югай <yanayugai6@gmail.com>
2024-02-01 17:40:31 +0300

    change http status

--- tests/test_follows.py
+++ tests/test_follows.py
@@ -1,70 +1,70 @@
-from tests.test_recipes import client
+import http
 
 
 # TODO: when db will be ready add assert response.json
-def test_add_follow():
+def test_add_follow(client):
     response = client.post(
         '/api/users/1/subscribe/',
         headers={"Authorization": "Token TOKENVALUE"},
     )
-    assert response.status_code == 201
+    assert response.status_code == http.HTTPStatus.CREATED
 
 
-def test_add_follow_twice():
+def test_add_follow_twice(client):
     response = client.post(
         '/api/recipes/1/subscribe/',
         headers={"Authorization": "Token TOKENVALUE"},
     )
-    assert response.status_code == 400
-    assert response.json == {
+    assert response.status_code == http.HTTPStatus.BAD_REQUEST
+    assert response.json() == {
         "errors": "string"
     }
 
 
-def test_add_follow_failure_credentials():
+def test_add_follow_failure_credentials(client):
     response = client.post(
         '/api/recipes/2/subscribe/',
         headers={"Authorization": "Token invalid_token"},
     )
-    assert response.status_code == 401
+    assert response.status_code == http.HTTPStatus.UNAUTHORIZED
     assert response.json() == {
         "detail": "Учетные данные не были предоставлены."
     }
 
 
-def test_delete_follow():
+def test_delete_follow(client):
     response = client.delete(
         '/api/recipes/2/subscribe/',
         headers={"Authorization": "Token TOKENVALUE"},
     )
-    assert response.status_code == 204
+    assert response.status_code == http.HTTPStatus.NO_CONTENT
 
 
-def test_delete_follow_twice():
+def test_delete_follow_twice(client):
     response = client.delete(
         '/api/recipes/2/subscribe/',
         headers={"Authorization": "Token TOKENVALUE"},
     )
-    assert response.status_code == 400
-    assert response.json == {
+    assert response.status_code == http.HTTPStatus.BAD_REQUEST
+    assert response.json() == {
         "errors": "string"
     }
 
 
-def test_delete_follow_failure_credentials():
+def test_delete_follow_failure_credentials(client):
     response = client.delete(
         '/api/recipes/2/subscribe//1/subscribe/',
         headers={"Authorization": "Token invalid_token"},
     )
-    assert response.status_code == 401
+    assert response.status_code == http.HTTPStatus.UNAUTHORIZED
     assert response.json() == {
         "detail": "Учетные данные не были предоставлены."
     }
 
 
-def test_get_follows():
+def test_get_follows(client):
     raise NotImplementedError
 
 
-def test_add_follows_on_me():
+def test_add_follows_on_me(client):
     raise NotImplementedError
