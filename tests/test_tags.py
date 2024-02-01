9340ba24546e5c1be1752bdeb88dbe01b3882a6c
Яна Югай <yanayugai6@gmail.com>
2024-02-01 17:40:44 +0300

    change http status

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -1,18 +1,14 @@
-from tests.test_recipes import client
-from tests.utils import test_nonexistent_objects
 from tests.example_responses import tags
+import http
 
 
-test_nonexistent_objects('/api/tags/7', client)
-
-
-def test_get_tags():
+def test_get_tags(client):
     response = client.get('/api/tags/')
-    assert response.status_code == 200
+    assert response.status_code == http.HTTPStatus.OK
     assert response.json == list(tags.values())
 
 
-def test_get_tag():
+def test_get_tag(client):
     response = client.get('/api/tags/1/')
-    assert response.status_code == 200
+    assert response.status_code == http.HTTPStatus.OK
     assert response.json == tags[1]
