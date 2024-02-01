ef5049f319491d6fa29cc9d71f550b860f9faf2c
Яна Югай <yanayugai6@gmail.com>
2024-02-01 18:01:57 +0300

    add test for permission

--- tests/test_common.py
+++ tests/test_common.py
@@ -7,3 +7,12 @@ def test_nonexistent_objects(url, client):
     response = client.get(url)
     assert response.status_code == http.HTTPStatus.NOT_FOUND, f'{url} доступен'
     assert response.json() == {"detail": "Страница не найдена."}
+
+
+@pytest.mark.parametrize("url", ['/api/recipes/download_shopping_cart/', '/api/users/subscriptions/'])
+def test_permission_denied(url, client):
+    response = client.get(url=url, headers={"Authorization": "Token invalid_token"})
+    assert response.status_code == http.HTTPStatus.UNAUTHORIZED, f'{url} доступен'
+    assert response.json() == {
+        "detail": "Учетные данные не были предоставлены."
+    }
