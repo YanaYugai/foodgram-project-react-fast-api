527f01157e23ef6531d6946751a46a57bb88a908
Яна Югай <yanayugai6@gmail.com>
2024-02-01 17:40:49 +0300

    change http status

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,48 +1,49 @@
-from tests.test_recipes import client
-from tests.utils import test_nonexistent_objects
-from tests.example_responses import users, users_create
+from tests.example_responses import users
+import http
 
 
-test_nonexistent_objects('/api/users/100/', client)
-
-
-def test_get_users():
+def test_get_users(client):
     raise NotImplementedError
 
 
-def test_get_profile():
+def test_get_profile(client):
     raise NotImplementedError
 
 
-def test_create_token():
+def test_create_token(client):
     raise NotImplementedError
 
 
-def test_delete_token():
+def test_delete_token(client):
     raise NotImplementedError
 
 
-def test_change_password():
+def test_change_password(client):
     raise NotImplementedError
 
 
-def test_get_user():
+def test_get_user(client):
     response = client.get('/api/users/1/')
-    assert response.status_code == 200
-    assert response.json == users[1]
+    assert response.status_code == http.HTTPStatus.OK
+    assert response.json() == users[1]
 
 
-def test_post_user():
+def test_post_user(client):
     response = client.post(
         '/api/users/',
         json={
-            "email": "dpupkin@yandex.ru",
-            "username": "dima.pupkin",
-            "first_name": "Дима",
+            "email": "vpupkin@yandex.ru",
+            "username": "vasya.pupkin",
+            "first_name": "Вася",
             "last_name": "Пупкин",
             "password": "Qwerty123"
         },
     )
-    last_value = list(users_create.values())[-1]
-    assert response.status_code == 201
-    assert response.json == users_create[last_value]
+    assert response.status_code == http.HTTPStatus.CREATED
+    assert response.json() == {
+        "email": "vpupkin@yandex.ru",
+        "id": 0,
+        "username": "vasya.pupkin",
+        "first_name": "Вася",
+        "last_name": "Пупкин"
+    }
