8902bfdc660b869471f97535db955f7808d09c7b
Яна Югай <yanayugai6@gmail.com>
2024-02-01 17:40:02 +0300

    change http status

--- tests/test_ingredients.py
+++ tests/test_ingredients.py
@@ -1,18 +1,14 @@
-from tests.test_recipes import client
-from tests.utils import test_nonexistent_objects
 from tests.example_responses import ingredients
+import http
 
 
-test_nonexistent_objects('/api/ingredients/2000/', client)
-
-
-def test_get_ingredients():
+def test_get_ingredients(client):
     response = client.get('/api/ingredients/')
-    assert response.status_code == 200
-    assert response.json == list(ingredients.values())
+    assert response.status_code == http.HTTPStatus.OK
+    assert response.json() == list(ingredients.values())
 
 
-def test_get_tag():
+def test_get_tag(client):
     response = client.get('/api/ingredients/1/')
-    assert response.status_code == 200
-    assert response.json == ingredients[1]
+    assert response.status_code == http.HTTPStatus.OK
+    assert response.json() == ingredients[1]
