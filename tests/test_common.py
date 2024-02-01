145267c3660c49f6a97fbc9fcaf5052f32f24e3f
Яна Югай <yanayugai6@gmail.com>
2024-02-01 17:53:23 +0300

    parametrize test

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,5 +1,8 @@
 import http
+import pytest
 
+
+@pytest.mark.parametrize("url", ['/api/recipes/100/', '/api/tags/100/', '/api/ingredients/1000/'])
 def test_nonexistent_objects(url, client):
     response = client.get(url)
     assert response.status_code == http.HTTPStatus.NOT_FOUND, f'{url} доступен'
