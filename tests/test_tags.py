7282512da7991fa5bc372f0c95e84dd4c91e1528
Яна Югай <yanayugai6@gmail.com>
2024-02-05 15:27:12 +0300

    fixed json to json()

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -5,10 +5,10 @@ import http
 def test_get_tags(client):
     response = client.get('/api/tags/')
     assert response.status_code == http.HTTPStatus.OK
-    assert response.json == list(tags.values())
+    assert response.json() == list(tags.values())
 
 
 def test_get_tag(client):
     response = client.get('/api/tags/1/')
     assert response.status_code == http.HTTPStatus.OK
-    assert response.json == tags[1]
+    assert response.json() == tags[1]
