95027ec92b7af21ea6f28870b009375004a85f19
Яна Югай <yanayugai6@gmail.com>
2024-02-08 20:42:59 +0300

    Add fixture for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,20 @@
 from fastapi.testclient import TestClient
 from backend.main import app
 from pytest import fixture
+from typing import Generator
+from sqlalchemy.orm import Session
+from backend.database import engine
 
 
-@fixture  # function? module?
-def client() -> TestClient:
-    return TestClient(app)
+@fixture(scope="session")
+def db() -> Generator:
+    with Session(engine) as session:
+        yield session
+
+
+@fixture(scope="module")
+def client() -> Generator:
+    with TestClient(app) as c:
+        yield c
+
+
