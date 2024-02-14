856043eedacd3ce9bc03d7dced64cc7a73777c94
Яна Югай <yanayugai6@gmail.com>
2024-02-14 17:13:38 +0300

    Add delete-function

--- backend/src/crud/services.py
+++ backend/src/crud/services.py
@@ -1,11 +1,14 @@
 from models import User
 from sqlalchemy import select
 from sqlalchemy. orm import Session
-from fastapi import HTTPException
+from fastapi import HTTPException, Depends, Header
+from http import HTTPStatus
 from users.schemas import UserTokenCreation, UserCreation
+from typing import Annotated
+from pydantic import BaseModel
 
 
-def get_object_by_id_or_error(id: int, session: Session, model):
+def get_object_by_id_or_error(id: int, session: Session, model: BaseModel):
     statement = select(model).where(model.id == id)
     obj = session.scalar(statement)
     if obj is None:
@@ -13,13 +16,13 @@ def get_object_by_id_or_error(id: int, session: Session, model):
     return obj
 
 
-def get_objects(session: Session, model):
+def get_objects(session: Session, model: BaseModel):
     statement = select(model)
     objects = session.scalars(statement)
-    return objects
+    return list(objects)
 
 
-def get_token_or_error(session: Session, data: UserTokenCreation):
+def create_token_or_error(session: Session, data: UserTokenCreation):
     email = data.get('email')
     password = data.get('password')
     query = select(User).where(User.email == email)
@@ -42,3 +45,12 @@ def create_user(session: Session, data: UserCreation):
     session.commit()
     return user
 
+
+#def create_post(token: str = Depends(get), Authorization: Annotated[str, Header()]):
+
+
+def delete_object(session: Session, id: int, authorization: dict = Depends(check_token), model: BaseModel):
+    obj = get_object_by_id_or_error(id, session, model)
+    session.delete(obj)
+    session.commit()
+    return HTTPStatus.NO_CONTENT
