399f86328668736b3cd6135c70c2a43adfadff73
Яна Югай <yanayugai6@gmail.com>
2024-02-18 18:39:14 +0300

    Add get_user and get_users tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,10 +1,12 @@
 from tests.example_responses import users
 import http
 from sqlalchemy.orm import Session
+from sqlalchemy import func, select
 import random
 import string
 from backend.src.users.schemas import UserCreation
-from backend.src.crud.services import create_user
+from backend.src.models import User
+from backend.src.crud.services import create_user, get_object_by_id_or_error, get_objects
 
 
 def random_lower_string() -> str:
@@ -15,7 +17,7 @@ def random_email() -> str:
     return f"{random_lower_string()}@{random_lower_string()}.com"
 
 
-def test_get_users(client):
+def test_get_users_response(client):
     raise NotImplementedError
 
 
@@ -35,7 +37,7 @@ def test_change_password(client):
     raise NotImplementedError
 
 
-def test_get_user(client):
+def test_get_user_response(client):
     response = client.get('/api/users/1/')
     assert response.status_code == http.HTTPStatus.OK
     assert response.json() == users[1]
@@ -75,3 +77,22 @@ def test_create_user(db: Session) -> None:
     assert user.first_name == first_name
     assert user.last_name == last_name
     assert hasattr(user, "password")
+
+
+def test_get_user(db: Session) -> None:
+    email = random_email()
+    password = random_lower_string()
+    username = random_lower_string()
+    first_name = random_lower_string()
+    last_name = random_lower_string()
+    user_in = UserCreation(email=email, password=password, username=username, first_name=first_name, last_name=last_name)
+    user = create_user(session=db, data=user_in)
+    user_check = get_object_by_id_or_error(user.id, db, User)
+    assert user.email == user_check.email
+    assert user.username == user_check.username
+
+
+def test_get_users(db: Session):
+    users = get_objects(db, User)
+    count_users = db.scalar(select(func.count(User.id)))
+    assert len(users) == count_users
