e783784dcd018deb88f6c5cd004352453538ef12
Яна Югай <yanayugai6@gmail.com>
2024-02-22 18:29:01 +0300

    Imports fixed, add crud-part

--- backend/src/users/routers.py
+++ backend/src/users/routers.py
@@ -1,30 +1,30 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 from src.users.schemas import (
-    AuthorRead,
     UserCreation,
     UserResponseCreation,
 )
-from src.example_responses import users  # noqa: E402
+from database import SessionApi
+from src.crud import services
+from src.models import User
+from typing import List
 
 
 router = APIRouter(prefix='/api/users', tags=['users'])
 
 
-@router.get('/{user_id}/', response_model=AuthorRead)
-def get_user(user_id: int):
-    if user_id not in users:
-        raise HTTPException(status_code=404, detail='Страница не найдена.')
-    return users.get(user_id)
+@router.get('/{user_id}/', response_model=UserResponseCreation)
+def get_user(user_id: int, session: SessionApi):
+    user = services.get_object_by_id_or_error(id=user_id, session=session, model=User)
+    return user
 
 
 @router.post('/', response_model=UserResponseCreation, status_code=201)
-def post_user(user: UserCreation):
-    return UserResponseCreation.model_validate(
-        {
-            "email": "vpupkin@yandex.ru",
-            "id": 0,
-            "username": "vasya.pupkin",
-            "first_name": "Вася",
-            "last_name": "Пупкин",
-        },
-    )
+def post_user(user: UserCreation, session: SessionApi):
+    user = services.create_user(session, user)
+    return user
+
+
+@router.get('/', response_model=List[UserResponseCreation])
+def get_users(session: SessionApi):
+    users = services.get_objects(session=session, model=User)
+    return users
