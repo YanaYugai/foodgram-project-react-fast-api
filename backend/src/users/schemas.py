733d8e3ab1f669509a82cc1ad717a2887db155e5
Яна Югай <yanayugai6@gmail.com>
2024-02-22 18:30:32 +0300

    Correct Models

--- backend/src/users/schemas.py
+++ backend/src/users/schemas.py
@@ -1,28 +1,27 @@
 from pydantic import BaseModel, EmailStr, field_validator, ConfigDict
 
 
-class UserResponseCreation(BaseModel):
+class UserBase(BaseModel):
     email: EmailStr
-    id: int
     username: str
     first_name: str
     last_name: str
+
+
+class UserResponseCreation(UserBase):
+    id: int
 
 
 class AuthorRead(UserResponseCreation):
     is_subscribed: bool
 
 
-class UserCreation(BaseModel):
-    email: EmailStr
-    username: str
-    first_name: str
-    last_name: str
+class UserCreation(UserBase):
     password: str
 
     model_config = ConfigDict(from_attributes=True)
 
-    @field_validator("username")  # pydantic v1
+    @field_validator("username")
     def validate_bad_words(cls, username: str):
         if username == "me":
             raise ValueError("bad username, choose another")
