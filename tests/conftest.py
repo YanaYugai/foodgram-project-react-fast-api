8ec82f1262a5ce8b6b20943377108035109cffd9
Яна Югай <yanayugai6@gmail.com>
2024-02-22 16:56:22 +0300

    Add function for clear all data after tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,15 +3,26 @@ from pytest import fixture
 from typing import Generator
 from sqlalchemy.orm import sessionmaker
 from main import app
-from database import engine
+from database import engine, Base
+from contextlib import contextmanager
+from sqlalchemy.sql import text
+
 
 TestSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def clear_tables():
+    with TestSession() as conn:
+        for table in Base.metadata.sorted_tables:
+            conn.execute(text(f'TRUNCATE "{table.name}" RESTART IDENTITY CASCADE;'))
+        conn.commit()
 
 
 @fixture(scope="session")
 def db() -> Generator:
     with TestSession() as session:
         yield session
+    clear_tables()
 
 
 @fixture(scope="module")
