f304bd186e30920e4af03fef5caa29a8c2cd11b5
Яна Югай <yanayugai6@gmail.com>
2024-02-22 18:27:55 +0300

    Imports fixed, errors have been corrected

--- backend/src/crud/services.py
+++ backend/src/crud/services.py
@@ -1,21 +1,21 @@
-from backend.src.models import User, Recipe
+from src.models import User, Recipe
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from fastapi import HTTPException
 from http import HTTPStatus
 from src.users.schemas import UserTokenCreation, UserCreation
 from src.recipes.schemas import RecipeCreate
-from pydantic import BaseModel
+from database import Base
 
 
-def get_object_by_id_or_error(id: int, session: Session, model: BaseModel):
+def get_object_by_id_or_error(id: int, session: Session, model: Base):
     obj = session.get(model, id)
     if obj is None:
         raise HTTPException(status_code=404, detail='Страница не найдена.')
     return obj
 
 
-def get_objects(session: Session, model: BaseModel):
+def get_objects(session: Session, model: Base):
     statement = select(model)
     objects = session.scalars(statement).all()
     return objects
@@ -57,7 +57,7 @@ def create_recipe(session: Session, data: RecipeCreate):
 def delete_object(
     session: Session,
     id: int,
-    model: BaseModel,
+    model: Base,
 ):
     obj = get_object_by_id_or_error(id, session, model)
     session.delete(obj)
