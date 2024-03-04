79d7f238c5455040c7820d27a665fb71dbbbe470
Яна Югай <yanayugai6@gmail.com>
2024-03-04 18:15:33 +0300

    Delete useless function

---
+++ backend/src/auth/routers.py
@@ -0,0 +1,41 @@
+from typing import Annotated, Optional
+
+from fastapi import APIRouter, Depends
+from fastapi.exceptions import HTTPException
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.security.utils import get_authorization_scheme_param
+from starlette.requests import Request
+from starlette.status import HTTP_401_UNAUTHORIZED
+
+from database import SessionApi
+from src.crud.services import get_user_by_email
+
+
+class OAuth2PasswordToken(OAuth2PasswordBearer):
+    async def __call__(self, request: Request) -> Optional[str]:
+        authorization = request.headers.get("Authorization")
+        scheme, param = get_authorization_scheme_param(authorization)
+        if not authorization or scheme.lower() != "token":
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=HTTP_401_UNAUTHORIZED,
+                    detail="Not authenticated",
+                    headers={"Authorization": "Token"},
+                )
+            else:
+                return None
+        return param
+
+
+router = APIRouter(prefix='/api/token', tags=['users'])
+
+oauth2_scheme = OAuth2PasswordToken(tokenUrl="api/token/login/")
+
+
+@router.post("/login/")
+async def login(
+    session: SessionApi,
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+):
+    user = get_user_by_email(session=session, data=form_data)
+    return {"access_token": user.username, "token_type": "bearer"}
--- backend/src/crud/services.py
+++ backend/src/crud/services.py
@@ -3,7 +3,6 @@ from typing import Any, List
 
 from fastapi import HTTPException
 from sqlalchemy import select
-from sqlalchemy.orm import Session
 
 from backend.src.models import Recipe, User
 from database import Base, SessionApi
@@ -13,7 +12,7 @@ from src.users.schemas import UserCreation, UserTokenCreation
 
 def get_object_by_id_or_error(
     id: int,
-    session: Session,
+    session: SessionApi,
     model: Base,
 ) -> Any:
     obj = session.get(model, id)
@@ -22,13 +21,26 @@ def get_object_by_id_or_error(
     return obj
 
 
-def get_objects(session: Session, model: Base) -> List[Base]:
+def get_objects(session: SessionApi, model: Base) -> List[Base]:
     statement = select(model)
     objects = session.scalars(statement).all()
     return objects
 
 
-def create_token_or_error(session: Session, data: UserTokenCreation):
+def get_user_by_email(session: SessionApi, data: UserTokenCreation) -> Any:
+    email = data.get('email')
+    password = data.get('password')
+    statement = select(User).where(
+        User.email == email,
+        User.password == password,
+    )
+    user = session.scalar(statement)
+    if user is None:
+        raise HTTPException(status_code=400, detail='Некорректные данные.')
+    return user
+
+
+def create_token_or_error(session: SessionApi, data: UserTokenCreation):
     email = data.get('email')
     password = data.get('password')
     query = select(User).where(User.email == email)
@@ -40,7 +52,7 @@ def create_token_or_error(session: Session, data: UserTokenCreation):
     #  return generate_token(user)
 
 
-def create_user(session: Session, data: UserCreation) -> Any:
+def create_user(session: SessionApi, data: UserCreation) -> Any:
     username = data.username
     statement = select(User).where(User.username == username)
     user = session.scalar(statement)
@@ -52,7 +64,7 @@ def create_user(session: Session, data: UserCreation) -> Any:
     return user
 
 
-def create_recipe(session: Session, data: RecipeCreate) -> Recipe:
+def create_recipe(session: SessionApi, data: RecipeCreate) -> Recipe:
     recipe = Recipe(data)
     session.add(recipe)
     session.commit()
@@ -60,7 +72,7 @@ def create_recipe(session: Session, data: RecipeCreate) -> Recipe:
 
 
 def delete_object(
-    session: Session,
+    session: SessionApi,
     id: int,
     model: Base,
 ) -> Any:
@@ -68,11 +80,3 @@ def delete_object(
     session.delete(obj)
     session.commit()
     return HTTPStatus.NO_CONTENT
-
-
-def check_token(session: SessionApi, token: str) -> User:
-    statement = select(User).where(User.token == token)
-    user = session.scalar(statement)
-    if user is None:
-        raise HTTPException(status_code=400, detail='Некорректные данные.')
-    return user
