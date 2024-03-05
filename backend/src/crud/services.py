b62ca1a8bac1f46224b2e603c977d6fb89911008
Яна Югай <yanayugai6@gmail.com>
2024-03-05 21:23:42 +0300

    Add token functionality

--- backend/src/crud/services.py
+++ backend/src/crud/services.py
@@ -1,12 +1,13 @@
 from http import HTTPStatus
-from typing import Any
+from typing import Annotated, Any
 
-from fastapi import HTTPException
+from fastapi import Depends, HTTPException
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
-from backend.src.models import User
+from backend.src.models import Token, User  # type: ignore
 from database import Base
+from src.auth.routers import oauth2_scheme
 from src.users.schemas import UserCreation
 
 
@@ -15,6 +16,17 @@ def get_object_by_id_or_error(id: int, session: Session, model: Base):
     if obj is None:
         raise HTTPException(status_code=404, detail='Страница не найдена.')
     return obj
+
+
+def get_object_by_email_or_error(email: str, password: str, session: Session):
+    statement = select(User).where(
+        User.email == email,
+        User.password == password,
+    )
+    user = session.scalar(statement)
+    if user is None:
+        raise HTTPException(status_code=404, detail='Страница не найдена.')
+    return user
 
 
 def get_objects(session: Session, model: Base):
@@ -35,12 +47,36 @@ def create_user(session: Session, data: UserCreation):
     return user
 
 
-def delete_object(
+def delete_object_by_id(
     session: Session,
     id: int,
     model: Base,
 ) -> Any:
     obj = get_object_by_id_or_error(id, session, model)
     session.delete(obj)
+    session.commit()
+    return HTTPStatus.NO_CONTENT
+
+
+def fake_decode_token(token):
+    return User(
+        username=token + "fakedecoded",
+        email="john@example.com",
+        full_name="John Doe",
+    )
+
+
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    user = fake_decode_token(token)
+    return user
+
+
+def delete_token(
+    session: Session,
+    token: Annotated[str, Depends(oauth2_scheme)],
+):
+    statement = select(Token).where(Token.access_token == token)
+    token = session.scalar(statement)
+    session.delete(token)
     session.commit()
     return HTTPStatus.NO_CONTENT
