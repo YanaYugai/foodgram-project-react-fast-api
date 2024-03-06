9a56af56c9cd2057639727dc1f30a07d3b88ff46
Яна Югай <yanayugai6@gmail.com>
2024-03-06 20:18:58 +0300

    Add authenticate_user

--- backend/src/auth/utils.py
+++ backend/src/auth/utils.py
@@ -1,4 +1,7 @@
+from crud.services import get_object_by_email_or_error
 from passlib.context import CryptContext
+
+from database import SessionApi
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -9,3 +12,12 @@ def verify_password(plain_password, hashed_password):
 
 def get_password_hash(password):
     return pwd_context.hash(password)
+
+
+def authenticate_user(session: SessionApi, email: str, password: str):
+    user = get_object_by_email_or_error(session=session, email=email)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
