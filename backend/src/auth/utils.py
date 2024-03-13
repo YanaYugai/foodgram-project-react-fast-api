681fc741b5b1f6f71cd44c7a2e45b7a5644bbbf0
Яна Югай <yanayugai6@gmail.com>
2024-03-13 18:54:11 +0300

    Add typing to utils

--- backend/src/auth/utils.py
+++ backend/src/auth/utils.py
@@ -24,9 +24,9 @@ def create_access_token(
     return encoded_jwt
 
 
-def verify_password(plain_password, hashed_password):
+def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
 
-def get_password_hash(password):
+def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
