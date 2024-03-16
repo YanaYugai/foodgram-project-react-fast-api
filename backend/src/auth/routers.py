a66dc5c36d6aa73743c8c73fbf5c86c9812a348c
Яна Югай <yanayugai6@gmail.com>
2024-03-16 20:59:03 +0300

    Move oauth_scheme from routes to utils

--- backend/src/auth/routers.py
+++ backend/src/auth/routers.py
@@ -1,20 +1,17 @@
 import os
 from datetime import datetime, timedelta, timezone
-from typing import Annotated, Optional
+from typing import Annotated
 
-from auth.schemas import TokenCreation
-from auth.utils import create_access_token
 from dotenv import load_dotenv
 from fastapi import APIRouter, Depends, status
 from fastapi.exceptions import HTTPException
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from fastapi.security.utils import get_authorization_scheme_param
-from models import Token
-from starlette.requests import Request
-from starlette.status import HTTP_401_UNAUTHORIZED
+from fastapi.security import OAuth2PasswordRequestForm
 
-from database import SessionApi
-from src.crud.services import authenticate_user, delete_token
+from backend.database import SessionApi
+from backend.src.auth.schemas import TokenCreation
+from backend.src.auth.utils import create_access_token, oauth2_scheme
+from backend.src.crud.services import authenticate_user, delete_token
+from backend.src.models import Token
 
 load_dotenv()
 
@@ -22,25 +19,7 @@ load_dotenv()
 MINUTES = os.getenv('MINUTES')
 
 
-class OAuth2PasswordToken(OAuth2PasswordBearer):
-    async def __call__(self, request: Request) -> Optional[str]:
-        authorization = request.headers.get("Authorization")
-        scheme, param = get_authorization_scheme_param(authorization)
-        if not authorization or scheme.lower() != "token":
-            if self.auto_error:
-                raise HTTPException(
-                    status_code=HTTP_401_UNAUTHORIZED,
-                    detail="Not authenticated",
-                    headers={"Authorization": "Token"},
-                )
-            else:
-                return None
-        return param
-
-
 router = APIRouter(prefix='/api/token', tags=['token'])
-
-oauth2_scheme = OAuth2PasswordToken(tokenUrl="api/token/login/")
 
 
 @router.post("/login/", response_model=TokenCreation)
@@ -61,7 +40,13 @@ def login(
         )
     if MINUTES is None:
         raise ValueError
-    expire = datetime.now(timezone.utc) + timedelta(minutes=int(MINUTES))
+    expire = (
+        datetime.now(timezone.utc)
+        + timedelta(
+            minutes=int(MINUTES),
+        )
+    ).timestamp()
+    expire = timedelta(seconds=expire)
     token = Token(
         access_token=create_access_token(
             subject=user.id,
--- backend/src/auth/utils.py
+++ backend/src/auth/utils.py
@@ -1,10 +1,15 @@
 import os
 from datetime import datetime, timedelta
-from typing import Any, Union
+from typing import Any, Optional, Union
 
 from dotenv import load_dotenv
+from fastapi.exceptions import HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from fastapi.security.utils import get_authorization_scheme_param
 from jose import jwt
 from passlib.context import CryptContext
+from starlette.requests import Request
+from starlette.status import HTTP_401_UNAUTHORIZED
 
 load_dotenv()
 
@@ -12,6 +17,25 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 ALGORITHM = "HS256"
+
+
+class OAuth2PasswordToken(OAuth2PasswordBearer):
+    async def __call__(self, request: Request) -> Optional[str]:
+        authorization = request.headers.get("Authorization")
+        scheme, param = get_authorization_scheme_param(authorization)
+        if not authorization or scheme.lower() != "token":
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=HTTP_401_UNAUTHORIZED,
+                    detail="Not authenticated",
+                    headers={"Authorization": "Token"},
+                )
+            else:
+                return None
+        return param
+
+
+oauth2_scheme = OAuth2PasswordToken(tokenUrl="api/token/login/")
 
 
 def create_access_token(
