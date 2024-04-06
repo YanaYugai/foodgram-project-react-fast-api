af51768c1bc6f510bc93533d70794708c1443e3e
Яна Югай <yanayugai6@gmail.com>
2024-04-06 19:20:55 +0300

    Fix error

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -4,7 +4,7 @@ from sqlalchemy import engine_from_config, pool
 
 from alembic import context
 from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
-from src.models import Base
+from src.models import Base  # type: ignore
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
