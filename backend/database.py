4f52687b06322bdfda6004f370748916f77c7eb2
Яна Югай <yanayugai6@gmail.com>
2024-04-06 15:51:11 +0300

    Fix error

--- backend/database.py
+++ backend/database.py
@@ -9,12 +9,11 @@ from sqlalchemy.orm import (
     sessionmaker,
 )
 
-from backend.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+from backend.config import DB_HOST, DB_NAME, DB_PASS, DB_USER
 
 SQLALCHEMY_DATABASE_URL = (
-    f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
 )
-
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
 )
